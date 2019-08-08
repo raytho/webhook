2e3310e643257df9adf46e568bfc70928c09f47a
raytho <pepperray01@gmail.com>
2019-08-08 10:31:28 -0500

    Se crea proyecto webhook

---
+++ WebHookApplication/WebHookHandlers/GitHubWebHookHandler.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+
+
+
+using Microsoft.AspNet.WebHooks;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebHookApplication.WebHookHandlers
+{
+    public class GitHubWebHookHandler : WebHookHandler
+    {
+        public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
+        {
+			// make sure we're only processing the intended type of hook
+			if("GitHub".Equals(receiver, System.StringComparison.CurrentCultureIgnoreCase))
+			{
+				// todo: replace this placeholder functionality with your own code
+				string action = context.Actions.First();
+				JObject incoming = context.GetDataOrDefault<JObject>();
+			}
+            
+            return Task.FromResult(true);
+        }
+    }
+}
