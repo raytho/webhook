a0d53f2380c7aaa2ce6f01bf9b94c4e9d8a183e7
raytho <pepperray01@gmail.com>
2019-08-09 08:30:06 -0500

    Se agrega el generic json webhook, para json genericos que entren

---
+++ GenericJsonWebHook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GenericJsonWebHook.WebApiApplication" Language="C#" %>
---
+++ GenericJsonWebHook/WebHookHandlers/GenericJsonWebHookHandler.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+
+
+
+using log4net;
+using Microsoft.AspNet.WebHooks;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericJsonWebHook.WebHookHandlers
+{
+    public class GenericJsonWebHookHandler : WebHookHandler
+    {
+        public GenericJsonWebHookHandler()
+        {
+            this.Receiver = "genericjson";
+        }
+        private static ILog log = LogManager.GetLogger("MainIn");
+
+        public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
+        {
+            JObject data = context.GetDataOrDefault<JObject>();
+
+            log.Debug("Webhook ejecutando...");
+
+            return Task.FromResult(true);
+        }
+    }
+}
